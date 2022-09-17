8d9008be6767febf04f8bebe38d8968b710c6479
JustinHemnath <justin.hemnath.96@gmail.com>
2022-09-17 19:51:11 +0530

    17/9

---
+++ src/features/modalSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const modalSlice = createSlice({
+    name: 'modalSlice',
+    initialState,
+    reducers: {
+
+    }
+});
+
+export const {
+
+} = modalSlice.actions
+
+export default modalSlice.reducer
--- src/projects/Amazon/Cart/Checkout.js
+++ src/projects/Amazon/Cart/Checkout.js
@@ -1,19 +1,27 @@
 import Button from '@mui/material/Button';
 import { useContext, useState } from 'react';
 import { AmazonContext } from './../AmazonContext';
-import Modal from '@mui/material/Modal';
+import { Modal } from 'flowbite-react';
 
 const Checkout = () => {
   const { state } = useContext(AmazonContext);
   const [modalOpen, setmodalOpen] = useState(false);
 
+  const handleModal = (e) => {
+    e.stopPropagation();
+    setmodalOpen(false);
+  };
+
   return (
-    <div className="bg-[#FEFFFE] p-5 h-[15em] basis-1/4 grid items-center">
-      <h1 className="text-center text-3xl font-bold">
-        TOTAL : <span className="ml-2 font-black">₹{state.totalPrice}</span>
+    <div
+      className='bg-[#FEFFFE] p-5 h-[15em] basis-1/4 grid items-center'
+      onClick={modalOpen ? handleModal() : null}
+    >
+      <h1 className='text-center text-3xl font-bold'>
+        TOTAL : <span className='ml-2 font-black'>₹{state.totalPrice}</span>
       </h1>
       <Button
-        variant="contained"
+        variant='contained'
         sx={{
           backgroundColor: '#FFD814',
           color: 'black',
@@ -21,18 +29,16 @@ const Checkout = () => {
           fontWeight: '600',
           '&:hover': { backgroundColor: '#FFD814' },
         }}
-        onClick={() => setmodalOpen(true)}>
+        onClick={() => setmodalOpen(true)}
+      >
         Proceed To Buy
       </Button>
-      <Modal
-        open={modalOpen}
-        onClose={() => setmodalOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
+      <Modal show={modalOpen}>
         <div
-          className="w-[40em] -translate-y-2/4 -translate-x-2/4
-                     bg-gray-300 border-2 border-gray-700 rounded-lg absolute top-1/2 left-1/2 p-16">
-          <h1 className="text-center text-3xl font-bold">
+          className='w-[40em] -translate-y-2/4 -translate-x-2/4
+                     bg-gray-300 border-2 border-gray-700 rounded-lg absolute top-1/2 left-1/2 p-16'
+        >
+          <h1 className='text-center text-3xl font-bold'>
             You may want to avoid buying from this site
           </h1>
         </div>
